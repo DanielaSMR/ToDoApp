fc88ec5988c2e4bd3d58d619386feb8ccdb11ab7
«DanielaSMR» <10828775@ieselgrao.org>
2025-01-15 12:07:09 +0100

    1.1v

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,8 @@ android {
 }
 
 dependencies {
-
+    implementation (libs.androidx.lifecycle.runtime.ktx.v231)
+    implementation (libs.androidx.runtime.livedata)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
